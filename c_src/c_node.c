c313257a4c5e9d55635b76b35db08c0ed462960a
Josh Adams <josh@isotope11.com>
2014-01-21 14:23:49 -0600

    Spawn a new process fo reach request
    This is just modelling spawning a new C process for each request...

--- c_src/c_node.c
+++ c_src/c_node.c
@@ -1,47 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 #include "erl_interface.h"
 #include "ei.h"
 
+#define NUM_THREADS 10
+
 typedef unsigned char byte;
 
-int main() {
-  ETERM *tuplep, *intp;
-  ETERM *fnp, *argp;
-  int res;
-  byte buf[100];
-  long allocated, freed;
+CvMat* img() {
+  char * path = "./priv/sample_images/ABST.00000000.tif";
+  IplImage *source = cvLoadImage( path, CV_LOAD_IMAGE_GRAYSCALE );
+  int width = 200;
+  int height = (width * (source->height)) / (source->width);
+  int param[3] = {CV_IMWRITE_PNG_COMPRESSION, 3, 0};
+  IplImage *destination = cvCreateImage(
+      cvSize( width, height ),
+      source->depth, source->nChannels );
+  cvResize( source, destination, CV_INTER_AREA );
+  return cvEncodeImage(".png", destination, param );
+}
 
+void handle_command(void *buf){
+}
+
+int main() {
+  byte buf[1000000];
   erl_init(NULL, 0);
 
-  while (read_cmd(buf) > 0) {
+  if (read_cmd(buf) > 0) {
+    ETERM *tuplep, *file;
+    ETERM *fnp;
     tuplep = erl_decode(buf);
     fnp = erl_element(1, tuplep);
-    argp = erl_element(2, tuplep);
+    CvMat* pngImage = img();
 
-    if (strncmp(ERL_ATOM_PTR(fnp), "foo", 3) == 0) {
-      res = foo(ERL_INT_VALUE(argp));
-    } else if (strncmp(ERL_ATOM_PTR(fnp), "bar", 17) == 0) {
-      res = bar(ERL_INT_VALUE(argp));
-    }
+    //if (strncmp(ERL_ATOM_PTR(fnp), "img", 3) == 0) {
+      // no-op for now?
+    //}
 
-    intp = erl_mk_int(res);
-    erl_encode(intp, buf);
-    write_cmd(buf, erl_term_len(intp));
+    file = erl_mk_binary((char *)pngImage->data.ptr, pngImage->cols);
+    erl_encode(file, buf);
+    write_cmd(buf, erl_term_len(file));
 
     erl_free_compound(tuplep);
     erl_free_term(fnp);
-    erl_free_term(argp);
-    erl_free_term(intp);
+    erl_free_term(file);
   }
-}
 
-int foo(int input)
-{
-  return input;
-}
-
-int bar(int input)
-{
-  return 20 + input;
+  return 0;
 }
--- src/complex.erl
+++ src/complex.erl
@@ -1,37 +1,35 @@
 -module(complex).
 -export([start/0, init/0]).
--export([foo/1, bar/1]).
+-export([img/1, benchmark/1, do_benchmark/2]).
 
 start() ->
   spawn(?MODULE, init, []).
 
 init() ->
-  register(complex, self()),
   process_flag(trap_exit, true),
   Port = open_port({spawn, 'priv/c_node'}, [{packet, 2}, binary]),
-  loop(Port).
+  loop(Port, self()).
 
-foo(X) ->
-  call_port({foo, X}).
-bar(Y) ->
-  call_port({bar, Y}).
+img(ReceiverPid) ->
+  Pid = start(),
+  ReceiverPid ! {data, call_port(Pid, {img})}.
 
-call_port(Msg) ->
-  complex ! {call, self(), Msg},
+call_port(Pid, Msg) ->
+  Pid ! {call, self(), Msg},
   receive
-    {complex, Result} ->
+    {_Pid, Result} ->
       Result
   end.
 
-loop(Port) ->
+loop(Port, Pid) ->
   receive
     {call, Caller, Msg} ->
       Port ! {self(), {command, term_to_binary(Msg)}},
       receive
         {Port, {data, Data}} ->
-          Caller ! {complex, binary_to_term(Data)}
+          Caller ! {Pid, binary_to_term(Data)}
       end,
-      loop(Port);
+      loop(Port, Pid);
 	stop ->
 	  Port ! {self(), close},
 	  receive
@@ -41,3 +39,18 @@ loop(Port) ->
     {'EXIT', _Port, _Reason} ->
       exit(port_terminated)
   end.
+
+%% This is just used to benchmark how many images we can do per second...
+benchmark(Number) ->
+  Pid = self(),
+  timer:tc(?MODULE, do_benchmark, [Pid, Number]).
+
+do_benchmark(_Pid, 0) ->
+  receive
+    {data, SomeData} -> do_benchmark(_Pid, 0)
+  after
+    1 -> ok
+  end;
+do_benchmark(Pid, Number) ->
+  spawn(?MODULE, img, [Pid]),
+  do_benchmark(Pid, Number-1).
